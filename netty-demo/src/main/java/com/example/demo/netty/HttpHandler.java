85590fcb649357d92e0cf396afdeb8c90ac42009
ItGeneral <799178171@qq.com>
2018-02-27 10:13:27 +0800

    edit properties

---
+++ netty-demo/src/main/java/com/example/demo/netty/HttpHandler.java
@@ -0,0 +1,9 @@
+package com.example.demo.netty;
+
+/**
+ * @author SongJiuHua.
+ * @Date Created on 2018/1/25.
+ * @description
+ */
+public class HttpHandler {
+}
---
+++ netty-demo/src/main/java/com/example/demo/netty/HttpServer.java
@@ -0,0 +1,9 @@
+package com.example.demo.netty;
+
+/**
+ * @author SongJiuHua.
+ * @Date Created on 2018/1/24.
+ * @description
+ */
+public class HttpServer {
+}
